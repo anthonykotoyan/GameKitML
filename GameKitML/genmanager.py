068338f4db4b92990af9d925eb61067365cef1ab
akotoy <148432161+anthonykotoyan@users.noreply.github.com>
2024-05-24 00:02:17 -0700

    Run_Gen and Reset_Gen added genmanager.py

--- GameKitML/genmanager.py
+++ GameKitML/genmanager.py
@@ -1,3 +1,5 @@
+import time
+
 from GameKitML.neuralnetwork import NeuralNetwork as nn
 
 
@@ -5,11 +7,21 @@ from GameKitML.neuralnetwork import NeuralNetwork as nn
 class Trainer:
 
     def __init__(self, Agent):
+
         # class of the Agent
-        self.Agent = Agent
         self.agents = []
+        self.Agent = Agent
+
+        # info user gives us (each has an assignation function)
         self.nn_info = None
         self.run_info = None
+        self.gen_info = None
+
+        # gen time
+        self.start_time = time.time()
+        self.gen_time = None
+
+
 
     # nn_Info creates the params/info for the nn class
     # layer is a list describing the size of the nn
@@ -25,6 +37,12 @@ class Trainer:
     # input_variable_name is the name of the variable that contains what the nn would see
     def Set_Run_Info(self, run_func, output_input_converter, input_variable_name):
         self.run_info = [run_func, output_input_converter, input_variable_name]
+
+    # gen_time is the amount of time in seconds the generation will last
+    # agent_death_variable_name is the name of the variable that shows if the Agent is dead or not
+    def Set_Gen_Info(self, gen_time, agent_death_variable_name, reset_agent, score_variable_name):
+        self.gen_time = gen_time
+        self.gen_info = [agent_death_variable_name, reset_agent, score_variable_name]
 
     # There is a class called NeuralNetwork which we imported as "nn"
     # The nn class contains the code to run and store the neural network for an individual Agent
@@ -56,12 +74,16 @@ class Trainer:
     # runs all the agents using the nn_info and the run_info
     def Run_Agents(self):
         for agent in self.agents:
+            # self.gen_info[0] is the name of the agent_death variable the user provided
+            is_dead = getattr(agent[0], self.gen_info[0], None)
+
+            if is_dead:
+                continue
             # this finds the variable name input_variable_name inside the Agent class and uses that as nn input
             nn_inputs = getattr(agent[0], self.run_info[2], None)
 
             # agent[1] would be the nn of the current looped over agent
             # nn_info[3] is the activation function used to get the outputs
-
             nn_outputs = agent[1].run(nn_inputs, self.nn_info[3])
 
             # convert nn outputs to the agent inputs
@@ -69,3 +91,24 @@ class Trainer:
 
             # run the agent using the inputs
             self.run_info[0](agent[0], agent_input)
+
+    # run the generation and restart if the gen time is passed
+    def Run_Gen(self):
+        if time.time() - self.start_time >= self.gen_time:
+            self.start_time = time.time()
+            self.Reset_Gen()
+        else:
+
+            self.Run_Agents()
+
+    # loop through all the agents and reset them using the reset_agent func the user provided
+    def Reset_Gen(self):
+        print("hi")
+        for agent in self.agents:
+            # gen_info[1] is the reset_agent func the user provided
+            self.gen_info[1](agent[0])
+
+            # not added yet but here we will figure out the agent with the highest score and mutate his network and shit
+            # self.gen_info[2] is the name of the agent_death variable the user provided
+            score = getattr(agent[0], self.gen_info[2], None)
+            
